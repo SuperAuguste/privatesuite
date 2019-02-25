39223fb345614c5b95478132c58af0ef8bad4a17
rebeccajane <rebjane@Rebeccas-MacBook-Pro.local>
2019-02-24 20:39:34 -0500

    Menu dropdown linklinks exist

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -5,10 +5,22 @@
             <!-- The title -->
             <h1> {{this.header}} </h1>
             <!-- The links -->
-            <div class="menu-items" v-for="(item, i) in this.links" :key="i">
-              <a :href="item.url" v-html="item.title" target="_blank"/>
+            <div class="menu-links-wrap">
+            <div class="menu-links">
+          
+                
+                <div class="links" v-for="(item, i) in this.links" :key="i">
+                  <a class="dropdown" @click.prevent="toggleDropdown()" :href="item.url" v-if="item.title == 'Issues'" v-html="item.title" target="_blank"/>
+                  <a :href="item.url" v-if="item.title != 'Issues'" v-html="item.title" target="_blank"/>
+                </div>
             </div>
-
+            
+                <div class="children-links-container" v-if="showDropdown == true">
+                  <div class="children-links" :style="{ width : computeDropdownSize(links.length) + '%'}" v-for="(item, i) in this.childrenLinks" :key="i" >
+                    <a :href="item.url" v-html="item.title" target="_blank"/>
+                </div>
+                </div>
+         </div>
          </div>
       </div>
 </template>
@@ -23,11 +35,14 @@ export default {
   data() {
     let header = ''
     let links = []
-    
-
+    let childrenLinks = []
+    let showDropdown = false
+  
       return {
+        header,
         links,
-        header
+        childrenLinks,
+        showDropdown
       }
   },
   //grabbing the values from wordpress, and assigning them to the variables
@@ -35,20 +50,35 @@ export default {
     linkFetch() {
       this.$http.get('wp-api-menus/v2/menus/27').then((response) => {
         this.links = response.body.items;
-        // console.log(this.links);
+
+        for(const i in this.links) {
+          //there is no error but it's being stupid :(
+          if (response.body.items[i].children.length > 0) {
+           this.childrenLinks = response.body.items[i].children;
+          }
+        }
+
       });
+    },
+  toggleDropdown() {
+      this.showDropdown = !this.showDropdown;
+      return this.showDropdown;
     },
  titleFetch() {
       this.$http.get('').then((response) => {
         this.header = response.body.name;
       });
-    }
+    },
+
+  computeDropdownSize(length) {
+      return length = 100 / length;
+    },
 
 
   },
   mounted() {
      this.linkFetch();
-    this.titleFetch();
+      this.titleFetch();
   },
   computed: { 
     // ...mapState ([
@@ -67,17 +97,32 @@ export default {
   position: relative;
   width: 100%;
 }
+.menu-links-wrap {
+
+  font-family: 'TSTAR Mono Round';
+  margin: 2em calc(100% / 11) 0px;
+}
+.menu-links{
+  display: flex;
+}
 a {
   text-decoration: none;
 }
-.menu-items {
-  font-family: 'TSTAR Mono Round';
+.links {
   position: relative;
   display: inline-block;
-  padding: 0em 3em;
+  /* padding: 0em 3em; */
+  margin: auto;
 }
 .link {
   position: relative;
+}
+.children-links-container {
+  padding-top: 1em;
+}
+.children-links {
+  display: block;
+  text-align: center;
 }
 h1 {
   text-transform: uppercase;
