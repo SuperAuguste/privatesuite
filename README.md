5b2b4bfba40da347649904cb6e7ccc6652a7b5ca
rebeccajane <rebjane@Rebeccas-MacBook-Pro.local>
2019-04-03 08:21:20 -0400

    merging?

--- README.md
+++ README.md
@@ -1,27 +1,34 @@
 # privatesuite
 
+This repository contains the source code for [Private Suite Magazine's website](https://privatesuitemag.com).
+
 ## Project setup
-```
+To setup this project
+
+```cmd
 yarn install
+
+yarn add stylus stylus-loader -g --save
+yarn add vuex -g --save
 ```
 
 ### Compiles and hot-reloads for development
-```
+```cmd
 yarn run serve
 ```
 
 ### Compiles and minifies for production
-```
+```cmd
 yarn run build
 ```
 
 ### Run your tests
-```
+```cmd
 yarn run test
 ```
 
 ### Lints and fixes files
-```
+```cmd
 yarn run lint
 ```
 
--- src/components/News.vue
+++ src/components/News.vue
@@ -10,10 +10,10 @@
                     <h1 class="title">News</h1>
                 </div>
                 <div class="post-title-wrap">
-                   <div class="image"/>
+                   <!-- <div class="image"/> -->
                     <transition class="post-title" v-for="(idx, i) in this.postBody" :key="i">
                       <div class="slider" v-if="sortedPosts(idx.link)">
-                       
+                       <div class="image"/>
                           <a :href="idx.link.replace('https://privatesuitemag.com', '')">
                         <div class="copy">
                           <h4 v-html="idx.title.rendered"/>
@@ -63,19 +63,29 @@ export default {
 
 @import "../stylesheets/styles.styl"
 .post-title-wrap 
-    // display flex
-    // flex-wrap wrap
+    display flex
+    flex-wrap wrap
     margin auto
 
 .post-wrapnth-child(even) > .slider
       width calc((2 / 9) * 100%)
 
-.image 
-    height 15em
-    width 5em
-    background-color pink
-    opacity .5
-    width calc((5 / 9) * 100%)
+.image {
+  height: 100%;
+  position: absolute;
+  width: 200%;
+  /* border: 3px solid lightgrey; */
+  background-color: pink;
+  opacity: .5;
+  width: calc((2 / 3) * 100%);
+}
+
+// .image 
+//     height 15em
+//     width 5em
+//     background-color pink
+//     opacity .5
+//     width calc((5 / 9) * 100%)
 
 .slider 
     width calc((5 / 9) * 100%)
@@ -99,7 +109,9 @@ export default {
 
 h4 
   text-align left
-
+.copy {
+  margin-left: calc(((1 / 9) * 100%) * 3);
+}
 
 @media screen and (max-width:768px)
   .slider,.image
